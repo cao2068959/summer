6f551f62f5614bd463edb4570663279b076e1703
guoyixing <904985041@qq.com>
2019-09-22 22:14:28 +0800

    aop功能调用接口

---
+++ src/main/java/com/chy/summer/framework/aop/aopalliance/intercept/Interceptor.java
@@ -0,0 +1,10 @@
+package com.chy.summer.framework.aop.aopalliance.intercept;
+
+import com.chy.summer.framework.aop.aopalliance.Advice;
+
+/**
+ * aop拦截器接口
+ */
+public interface Interceptor extends Advice {
+
+}
