73689413430c98febbf36210e9acf164ad942870
guoyixing <904985041@qq.com>
2019-09-26 21:19:36 +0800

    更新MethodInvocationProceedingJoinPoint

--- src/main/java/com/chy/summer/framework/aop/aspectj/MethodInvocationProceedingJoinPoint.java
+++ src/main/java/com/chy/summer/framework/aop/aspectj/MethodInvocationProceedingJoinPoint.java
@@ -29,25 +29,30 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 	 */
 	private final ProxyMethodInvocation methodInvocation;
 
+	/**
+	 * 方法执行参数的缓存
+	 */
 	@Nullable
 	private Object[] defensiveCopyOfArgs;
 
-	/** Lazily initialized signature object */
+	/**
+	 * 延迟初始化的签名对象
+	 */
 	@Nullable
 	private Signature signature;
 
-	/** Lazily initialized source location object */
+	/**
+	 * 延迟初始化的源位置对象
+	 */
 	@Nullable
 	private SourceLocation sourceLocation;
 
 
 	/**
-	 * Create a new MethodInvocationProceedingJoinPoint, wrapping the given
-	 * Spring ProxyMethodInvocation object.
-	 * @param methodInvocation the Spring ProxyMethodInvocation object
+	 * 创建一个新的MethodInvocationProceedingJoinPoint
 	 */
 	public MethodInvocationProceedingJoinPoint(ProxyMethodInvocation methodInvocation) {
-		Assert.notNull(methodInvocation, "MethodInvocation must not be null");
+		Assert.notNull(methodInvocation, "MethodInvocation不能为空");
 		this.methodInvocation = methodInvocation;
 	}
 
@@ -56,6 +61,9 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * 使用方法执行器的克隆对象执行方法
+	 */
 	@Override
 	public Object proceed() throws Throwable {
 		return this.methodInvocation.invocableClone().proceed();
@@ -63,18 +71,18 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 
 	@Override
 	public Object proceed(Object[] arguments) throws Throwable {
-		Assert.notNull(arguments, "Argument array passed to proceed cannot be null");
+		Assert.notNull(arguments, "传递来的参数数组不能为空");
 		if (arguments.length != this.methodInvocation.getArguments().length) {
-			throw new IllegalArgumentException("Expecting " +
-					this.methodInvocation.getArguments().length + " arguments to proceed, " +
-					"but was passed " + arguments.length + " arguments");
+			throw new IllegalArgumentException("执行方法的参数为 " +
+					this.methodInvocation.getArguments().length + "个, " +
+					"但是传递来的参数只有" + arguments.length + " 个");
 		}
 		this.methodInvocation.setArguments(arguments);
 		return this.methodInvocation.invocableClone(arguments).proceed();
 	}
 
 	/**
-	 * Returns the Spring AOP proxy. Cannot be {@code null}.
+	 * 获取aop的代理对象，并给不可以为空
 	 */
 	@Override
 	public Object getThis() {
@@ -82,7 +90,7 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 	}
 
 	/**
-	 * Returns the Spring AOP target. May be {@code null} if there is no target.
+	 * 获取AOP的目标对象。 如果没有目标，则可能为空
 	 */
 	@Override
 	@Nullable
@@ -93,13 +101,17 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 	@Override
 	public Object[] getArgs() {
 		if (this.defensiveCopyOfArgs == null) {
+			//获取方法执行的参数
 			Object[] argsSource = this.methodInvocation.getArguments();
+			//初始化参数缓存
 			this.defensiveCopyOfArgs = new Object[argsSource.length];
+			//拷贝参数
 			System.arraycopy(argsSource, 0, this.defensiveCopyOfArgs, 0, argsSource.length);
 		}
 		return this.defensiveCopyOfArgs;
 	}
 
+	//获取签名
 	@Override
 	public Signature getSignature() {
 		if (this.signature == null) {
@@ -108,6 +120,10 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 		return signature;
 	}
 
+	/**
+	 * 获取源位置
+	 * @return
+	 */
 	@Override
 	public SourceLocation getSourceLocation() {
 		if (this.sourceLocation == null) {
@@ -116,6 +132,10 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 		return this.sourceLocation;
 	}
 
+	/**
+	 * 获取类
+	 * @return METHOD_EXECUTION表示方法执行器
+	 */
 	@Override
 	public String getKind() {
 		return ProceedingJoinPoint.METHOD_EXECUTION;
@@ -123,7 +143,7 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 
 	@Override
 	public int getId() {
-		// TODO: It's just an adapter but returning 0 might still have side effects...
+		// 适配器
 		return 0;
 	}
 
@@ -149,48 +169,75 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 
 
 	/**
-	 * Lazily initialized MethodSignature.
+	 * 延迟初始化的Method签名
 	 */
 	private class MethodSignatureImpl implements MethodSignature {
 
+		/**
+		 * 参数名称
+		 */
 		@Nullable
 		private volatile String[] parameterNames;
 
+		/**
+		 * 获取方法名
+		 */
 		@Override
 		public String getName() {
 			return methodInvocation.getMethod().getName();
 		}
 
+		/**
+		 * 获取修饰类型
+		 */
 		@Override
 		public int getModifiers() {
 			return methodInvocation.getMethod().getModifiers();
 		}
 
+		/**
+		 * 获取声明类型
+		 */
 		@Override
 		public Class<?> getDeclaringType() {
 			return methodInvocation.getMethod().getDeclaringClass();
 		}
 
+		/**
+		 * 获取声明类型名称
+		 */
 		@Override
 		public String getDeclaringTypeName() {
 			return methodInvocation.getMethod().getDeclaringClass().getName();
 		}
 
+		/**
+		 * 获取返回类型
+		 */
 		@Override
 		public Class<?> getReturnType() {
 			return methodInvocation.getMethod().getReturnType();
 		}
 
+		/**
+		 * 获取执行方法
+		 */
 		@Override
 		public Method getMethod() {
 			return methodInvocation.getMethod();
 		}
 
+		/**
+		 * 获取参数类型
+		 */
 		@Override
 		public Class<?>[] getParameterTypes() {
 			return methodInvocation.getMethod().getParameterTypes();
 		}
 
+		/**
+		 * 获取参数名称
+		 */
 		@Override
 		@Nullable
 		public String[] getParameterNames() {
@@ -200,6 +247,9 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 			return this.parameterNames;
 		}
 
+		/**
+		 * 获取抛出异常的类型
+		 */
 		@Override
 		public Class<?>[] getExceptionTypes() {
 			return methodInvocation.getMethod().getExceptionTypes();
@@ -242,6 +292,9 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 			return sb.toString();
 		}
 
+		/**
+		 * 处理toString中的字符串
+		 */
 		private void appendTypes(StringBuilder sb, Class<?>[] types, boolean includeArgs,
 				boolean useLongReturnAndArgumentTypeName) {
 
@@ -273,15 +326,16 @@ public class MethodInvocationProceedingJoinPoint implements ProceedingJoinPoint,
 
 
 	/**
-	 * Lazily initialized SourceLocation.
+	 * 延迟初始化的源位置
 	 */
 	private class SourceLocationImpl implements SourceLocation {
 
 		@Override
 		public Class<?> getWithinType() {
 			if (methodInvocation.getThis() == null) {
-				throw new UnsupportedOperationException("No source location joinpoint available: target is null");
+				throw new UnsupportedOperationException("没有可用的连接点：目标为null");
 			}
+			//当前连接点的类型
 			return methodInvocation.getThis().getClass();
 		}
 
