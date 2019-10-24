efd6cdeeca6dcd0e1020b9baf934d97f0c745c0d
Grampus <704188931@qq.com>
2019-10-24 23:50:07 +0800

    日常

--- frame/src/main/java/com/chy/summer/framework/context/annotation/ConfigurationClassParser.java
+++ frame/src/main/java/com/chy/summer/framework/context/annotation/ConfigurationClassParser.java
@@ -185,6 +185,42 @@ public class ConfigurationClassParser {
         return null;
     }
 
+    private Set<MethodMetadata> retrieveBeanMethodMetadata(SourceClass sourceClass) {
+        AnnotationMetadata annotationMetadata = sourceClass.getMetadata();
+        Set<MethodMetadata> beanMethods = annotationMetadata.getAnnotatedMethods(Bean.class.getName());
+        if (beanMethods.size() > 1 && original instanceof StandardAnnotationMetadata) {
+            // Try reading the class file via ASM for deterministic declaration order...
+            // Unfortunately, the JVM's standard reflection returns methods in arbitrary
+            // order, even between different runs of the same application on the same JVM.
+            try {
+                AnnotationMetadata asm =
+                        this.metadataReaderFactory.getMetadataReader(original.getClassName()).getAnnotationMetadata();
+                Set<MethodMetadata> asmMethods = asm.getAnnotatedMethods(Bean.class.getName());
+                if (asmMethods.size() >= beanMethods.size()) {
+                    Set<MethodMetadata> selectedMethods = new LinkedHashSet<>(asmMethods.size());
+                    for (MethodMetadata asmMethod : asmMethods) {
+                        for (MethodMetadata beanMethod : beanMethods) {
+                            if (beanMethod.getMethodName().equals(asmMethod.getMethodName())) {
+                                selectedMethods.add(beanMethod);
+                                break;
+                            }
+                        }
+                    }
+                    if (selectedMethods.size() == beanMethods.size()) {
+                        // All reflection-detected methods found in ASM method set -> proceed
+                        beanMethods = selectedMethods;
+                    }
+                }
+            }
+            catch (IOException ex) {
+                logger.debug("Failed to read class file via ASM for determining @Bean method order", ex);
+                // No worries, let's continue with the reflection metadata we started with...
+            }
+        }
+        return beanMethods;
+    }
+
+
     /**
      * 配置类上面 @Import 注解的 解析
      * @param configClass
@@ -271,7 +307,7 @@ public class ConfigurationClassParser {
             return result;
         }
 
-        public ClassMetadata getMetadata() {
+        public AnnotationMetadata getMetadata() {
             return metadata;
         }
 
