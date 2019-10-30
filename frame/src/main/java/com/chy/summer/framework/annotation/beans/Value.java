2a5ed06ae5cec584961382a7e9eac809014522e7
Grampus <704188931@qq.com>
2019-10-31 00:12:50 +0800

    Autowired 实现

---
+++ frame/src/main/java/com/chy/summer/framework/annotation/beans/Value.java
@@ -0,0 +1,12 @@
+package com.chy.summer.framework.annotation.beans;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Value {
+
+    String value();
+
+}
