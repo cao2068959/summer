19a405acf8b3dea96cb89573d83e6537a960d6f4
DESKTOP-HCI6PF0\90498 <904985041@qq.com>
2019-12-10 10:46:18 +0800

    client 完成

--- link/src/main/java/com/chy/summer/link/client/impl/CookieStoreImpl.java
+++ link/src/main/java/com/chy/summer/link/client/impl/CookieStoreImpl.java
@@ -11,18 +11,32 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.function.Consumer;
 
 /**
- * TODO 这里继续
+ * Cookie储存管理的实现
  */
 public class CookieStoreImpl implements CookieStore {
-
+  /**
+   * 没有域的cookie
+   */
   private ConcurrentHashMap<Key, Cookie> noDomainCookies;
+  /**
+   * 带域的cookie
+   */
   private ConcurrentSkipListMap<Key, Cookie> domainCookies;
   
   public CookieStoreImpl() {
     noDomainCookies = new ConcurrentHashMap<>();
     domainCookies = new ConcurrentSkipListMap<>();
   }
-  
+
+  /**
+   * 返回一个cookie的迭代器，用于作为参数传递的过滤器
+   * 实现在cookie存储中选一个适当的cookie返回并清理path
+   *
+   * @param ssl 如果使用ssl，则返回true
+   * @param domain 正在使用的域
+   * @param path 正在使用的path
+   * @return 匹配到的cookie
+   */
   @Override
   public Iterable<Cookie> get(Boolean ssl, String domain, String path) {
     assert domain != null && domain.length() > 0;
@@ -30,28 +44,32 @@ public class CookieStoreImpl implements CookieStore {
     String cleanPath;
     {
       String uri = HttpUtils.removeDots(path);
-      // Remoe query params if present
+      // 删除掉查询条件
       int pos = uri.indexOf('?');
       if (pos > -1) {
         uri = uri.substring(0, pos);
       }
   
-      // Remoe frament identifier if present
+      // 如果有分片标识符，就删除
       pos = uri.indexOf('#');
       if (pos > -1) {
         uri = uri.substring(0, pos);
       }
       cleanPath = uri;
     }
-    
+
+    //有序map
     TreeMap<String, Cookie> matches = new TreeMap<>();
-    
+
+    //处理cookie的函数
     Consumer<Cookie> adder = c -> {
       if ( !Boolean.TRUE.equals(ssl) && c.isSecure()) {
+        //没使用ssl，并且是安全的
         return;
       }
       if (c.path() != null && !cleanPath.equals(c.path())) {
         String cookiePath = c.path();
+        //结尾添加“/”
         if (!cookiePath.endsWith("/")) {
           cookiePath += '/';
         }
@@ -59,9 +77,11 @@ public class CookieStoreImpl implements CookieStore {
           return;
         }
       }
+      //添加匹配
       matches.put(c.name(), c);      
     };
-    
+
+    //对每个没有域的cookie进行处理
     for (Cookie c : noDomainCookies.values()) {
       adder.accept(c);
     }
@@ -81,6 +101,14 @@ public class CookieStoreImpl implements CookieStore {
     return matches.values();
   }
 
+  /**
+   * 添加一个cookie到这个{@code CookieStore}
+   *
+   * 如果从服务器收到了相同名称的cookie，会覆盖掉同名参数值
+   *
+   * @param cookie 需要加入的{@link Cookie}
+   * @return 返回当前设置的对象，用于链式调用
+   */
   @Override
   public CookieStore put(Cookie cookie) {
     Key key = new Key(cookie.domain(), cookie.path(), cookie.name());
@@ -92,6 +120,12 @@ public class CookieStoreImpl implements CookieStore {
     return this;
   }
 
+  /**
+   * 删除之前添加的cookie
+   *
+   * @param cookie 需要删除的{@link Cookie}
+   * @return 返回当前设置的对象，用于链式调用
+   */
   @Override
   public CookieStore remove(Cookie cookie) {
     Key key = new Key(cookie.domain(), cookie.path(), cookie.name());
--- link/src/main/java/com/chy/summer/link/client/impl/SessionAwareInterceptor.java
+++ link/src/main/java/com/chy/summer/link/client/impl/SessionAwareInterceptor.java
@@ -13,22 +13,31 @@ import java.net.URI;
 import java.util.List;
 
 /**
- * TODO 这里继续
+ * session的拦截器
  */
 public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
 
+  /**
+   * header上下文的key
+   */
   private static final String HEADERS_CONTEXT_KEY = "_originalHeaders";
-  
+
+  /**
+   * 处理器
+   */
   @Override
   public void handle(HttpContext<?> context) {
     switch(context.phase()) {
     case PREPARE_REQUEST:
+      //尚未创建HttpClientRequest实例的阶段
       prepareRequest(context);
       break;
     case FOLLOW_REDIRECT:
+      //接收到HttpClientResponse，可能需要重定向
       processRedirectCookies(context);
       break;
     case DISPATCH_RESPONSE:
+      //response已创建完成，即将进行处理
       processResponse(context);
 	    break;
     default:
@@ -38,92 +47,123 @@ public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
     context.next();
   }
 
+  /**
+   * 尚未创建HttpClientRequest实例的阶段
+   */
   private void prepareRequest(HttpContext<?> context) {
-    
+    //获取请求实例
     HttpRequestImpl<?> request = (HttpRequestImpl<?>) context.request();
+    //获取web客户端
     WebClientSessionAware webclient = (WebClientSessionAware) request.client;
-
+    //找到header
     MultiMap headers = context.get(HEADERS_CONTEXT_KEY);
     if (headers == null) {
+      //没有header，就用请求的header生成一个CaseInsensitiveHeaders（不分大小写的header）
       headers = new CaseInsensitiveHeaders().addAll(request.headers());
+      //存入上下文
       context.set(SessionAwareInterceptor.HEADERS_CONTEXT_KEY, headers);
     }
     
-    // we need to reset the headers at every "send" because cookies can be changed,
-    // either by the server (that sent new ones) or by the user.
+    //每次发送之前重置一下header，如何添加请求头和客户端的请求头
     request.headers().clear().addAll(headers).addAll(webclient.headers());
 
+    //网络地址
     String domain = request.virtualHost;
     if (domain == null) {
       domain = request.host;
     }
-    
+    //cookie的配置
     Iterable<Cookie> cookies = webclient.cookieStore().get(request.ssl, domain, request.uri);
     for (Cookie c : cookies) {
       request.headers().add("cookie", ClientCookieEncoder.STRICT.encode(c));
     }
   }
 
+  /**
+   * 接收到HttpClientResponse的阶段，可能需要重定向
+   */
   private void processRedirectCookies(HttpContext<?> context) {
+    //处理重定向的响应
     this.processRedirectResponse(context);
+    //准备重定向请求
     this.prepareRedirectRequest(context);
   }
 
+  /**
+   * 处理重定向的响应
+   */
   private void processRedirectResponse(HttpContext<?> context) {
-    // Now the context contains the redirect request in clientRequest() and the original request in request()
+    //这个时候 context在clientRequest()中包含了重定向请求，而在request()中包含原始请求
     List<String> cookieHeaders = context.clientResponse().cookies();
     if (cookieHeaders == null) {
       return;
     }
 
+    //获取web客户端
     WebClientSessionAware webclient = (WebClientSessionAware) ((HttpRequestImpl)context.request()).client;
     HttpRequestImpl<?> originalRequest = (HttpRequestImpl<?>) context.request();
+    //获取cookie的储存
     CookieStore cookieStore = webclient.cookieStore();
+    //从请求中获取host
     String domain = URI.create(context.clientResponse().request().absoluteURI()).getHost();
     if (domain.equals(originalRequest.host) && originalRequest.virtualHost != null) {
+      //虚拟主机地址
       domain = originalRequest.virtualHost;
     }
+    //最终的主机地址
     final String finalDomain = domain;
     cookieHeaders.forEach(header -> {
       Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
       if (cookie != null) {
         if (cookie.domain() == null) {
-          // Set the domain if missing, because we need to send cookies
-          // only to the domains we received them from.
+          // 如果缺少域名，就设置域，用来发送Cookie
           cookie.setDomain(finalDomain);
         }
+        //cookie存入coolie存储中
         cookieStore.put(cookie);
       }
     });
   }
 
+  /**
+   * 准备重定向请求
+   */
   private void prepareRedirectRequest(HttpContext<?> context) {
-    // Now the context contains the redirect request in clientRequest() and the original request in request()
+    //这个时候 context在clientRequest()中包含了重定向请求，而在request()中包含原始请求
     HttpClientRequest redirectRequest = context.clientRequest();
     HttpRequestImpl<?> originalRequest = (HttpRequestImpl<?>) context.request();
     WebClientSessionAware webclient = (WebClientSessionAware) originalRequest.client;
-
+    //获取请求头
     MultiMap headers = context.get(HEADERS_CONTEXT_KEY);
     if (headers == null) {
+      //封装请求头
       headers = new CaseInsensitiveHeaders().addAll(redirectRequest.headers());
       context.set(SessionAwareInterceptor.HEADERS_CONTEXT_KEY, headers);
     }
 
+    //获取host
     String redirectHost = URI.create(redirectRequest.absoluteURI()).getHost();
     String domain;
     if (redirectHost.equals(originalRequest.host) && originalRequest.virtualHost != null) {
+      //使用虚拟主机地址
       domain = originalRequest.virtualHost;
     } else {
+      //使用原主机地址
       domain = redirectHost;
     }
 
+    //处理cookie
     Iterable<Cookie> cookies = webclient.cookieStore().get(originalRequest.ssl, domain, redirectRequest.path());
     for (Cookie c : cookies) {
       redirectRequest.headers().add("cookie", ClientCookieEncoder.STRICT.encode(c));
     }
   }
 
+  /**
+   * response已创建完成阶段
+   */
   private void processResponse(HttpContext<?> context) {
+    //处理一下cookie
     List<String> cookieHeaders = context.clientResponse().cookies();
     if (cookieHeaders == null) {
       return;
@@ -136,8 +176,7 @@ public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
       Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
       if (cookie != null) {
         if (cookie.domain() == null) {
-          // Set the domain if missing, because we need to send cookies
-          // only to the domains we received them from.
+          // 如果缺少域名，就设置域，用来发送Cookie
           cookie.setDomain(request.virtualHost != null ? request.virtualHost : request.host);
         }
         cookieStore.put(cookie);
